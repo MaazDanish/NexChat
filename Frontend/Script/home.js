91d4a2636362220407ae8102572101d2202c8d4d
MaazDanish <dk599318@gmail.com>
2024-01-03 19:11:15 +0530

    few bugs fixed in front end

--- Frontend/Script/home.js
+++ Frontend/Script/home.js
@@ -19,28 +19,85 @@ async function SignUp(event) {
         name: event.target.name.value,
         email: event.target.email.value,
         phoneNumber: event.target.phoneNumber.value,
-        password: event.target.password.value
+        password: event.target.password.value,
+        confirmPassword: event.target.confirmPassword.value
     }
     try {
-        const user = await axios.post('http://localhost:4106/nexchat/user/sign-up', userInfo);
-        if (user.status === 200) {
-            alert('registration is done')
+        var msg = document.getElementById('signupmsg');
+        if (isValidPassword(event.target.password.value, event.target.confirmPassword.value)) {
+            const user = await axios.post('http://localhost:4106/nexchat/user/sign-up', userInfo);
+            if (user.status === 200) {
+                alert('registration is done')
+                msg.textContent = 'Your ragistration is done successfully';
+            }
+            expires(msg);
+            clearField(event);
+        } else {
+            if (event.target.password.value.length < 6) {
+                // console.log('Password must be more than 6 letter');
+                msg.textContent = 'Error - Password must be more than 6 letter'
+                expires(msg);
+
+            } else if (event.target.password.value !== event.target.confirmpassword.value) {
+                // console.log('password does not match');
+                msg.textContent = "Error - password doesn't match";
+                expires(msg);
+
+            } else {
+                // console.log('Password should contain at least one small letter one capital letter and one numeric number');
+                msg.textContent = 'Error - Password must contain atleast one small letter, one capital letter and one numeric number'
+                expires(msg);
+
+            }
         }
-        clearField(event);
+
     } catch (err) {
         console.log(err);
         console.log(err.response.status);
-        if(err.response.status === 409){
+        if (err.response.status === 409) {
             alert('you already are member of our comunity. please sign in ')
+            msg.textContent = 'This user already registered with us';
+            expires(msg);
         }
     }
 }
+function isValidPassword(password, confirmPassword) {
+    if (password !== confirmPassword) {
+        return false;
+    }
+    if (password.length < 6) {
+        return false;
+    }
+    // check for the lower case letter
+    if (!/[a-z]/.test(password)) {
+        return false;
+    }
 
+    // Check for uppercase letter
+    if (!/[A-Z]/.test(password)) {
+        return false;
+    }
+
+    // Check for number
+    if (!/\d/.test(password)) {
+        return false;
+    }
+
+    // if all condition are true then 
+    return true;
+}
+
+function expires(msg) {
+    setTimeout(() => {
+        msg.textContent = '';
+    }, 3000)
+}
 function clearField(event) {
     event.target.name.value = '';
     event.target.email.value = '';
     event.target.phoneNumber.value = '';
     event.target.password.value = '';
+    event.target.confirmPassword.value = '';
 }
 
 // Sign In
@@ -60,8 +117,24 @@ async function SignIn(event) {
             alert('signin is done')
         }
         clearFields(event);
-    } catch (Err) {
-        console.log(Err);
+    } catch (res) {
+        console.log(res);
+        var msg = document.getElementById('signinmsg');
+        if (res.response.status === 404) {
+            msg.textContent = 'User Does not exist. Please sign up first'
+
+        }
+        else if (res.response.status === 401) {
+            msg.textContent = 'One or more field is incorrect. Try again'
+
+        } else {
+
+            msg.textContent = 'Internal server error'
+        }
+
+        setTimeout(() => {
+            msg.textContent = '';
+        }, 3000)
     }
 }
 
